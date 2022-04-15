2d3de32b2f5bb9b364f93c1803d0d5f326f20835
Aryan Patel <aroo2001@gmail.com>
2022-04-15 16:12:13 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./db");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+// const  = require("./routes/");
+
+
+app.use(cors({credentials: true, origin: "*"}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+connectDB();
+
+// app.use('/', );
+
+const portNumber = 4000;
+app.listen(portNumber, function () {
+  console.log("Example app listening on port " + portNumber);
+});
+
