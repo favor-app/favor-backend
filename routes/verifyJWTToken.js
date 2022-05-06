d0df80bdd9361516035fed3ef56c01c08366bff7
Aryan Patel <aroo2001@gmail.com>
2022-05-05 23:17:04 -0700

    Logout Endpoint

--- routes/verifyJWTToken.js
+++ routes/verifyJWTToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function(req,res,next){
     const token = req.cookies.jwt;
-    console.log(token);
+    console.log("Token: ",token);
     if(!token) return res.status(401).send('Access Denied');
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
