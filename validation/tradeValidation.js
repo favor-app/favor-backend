966a8a21068e162e75bd765681cafbb4fe7a4fa9
Sanchit Agarwal <sanchit1591@g.ucla.edu>
2022-05-06 15:39:54 -0700

    validation changed

--- validation/tradeValidation.js
+++ validation/tradeValidation.js
@@ -4,7 +4,7 @@ const Joi = require('@hapi/joi');
 // Register Validation 
 const tradeValidation = data => {
     const schema = Joi.object().keys({
-        favorerId: Joi.string().min(6).required(),
+        favorerId: Joi.string().min(6),
         favorId: Joi.string().min(6).required()
       });
     return schema.validate(data);
