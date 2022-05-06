5fe74e69ab00059c1507894b715c6347e4b3cff1
Aryan Patel <aroo2001@gmail.com>
2022-05-05 23:22:30 -0700

    Logout

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const verify = require("./verifyJWTToken");
 
 // Validate register and login fields
 const {
@@ -70,7 +71,16 @@ router.post("/login", async (req, res) => {
         // expires: new Date(new Date().getTime()+60*24*36000),
         jwtToken: token,
         // You can't access these tokens in the client's javascript
-        httpOnly: true
+        httpOnly: true,
     }).send("Successfully Logged In");
 });
+
+router.get("/logout", verify, async (req, res) => {
+    try {
+        res.clearCookie("jwt").send("Successfully Logged out");
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
 module.exports = router;
